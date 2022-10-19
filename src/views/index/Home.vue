ca9216156fb627b397e0a1d96f0e5b39a9e48d2c
qiudaoermu <793385821@qq.com>
2022-10-19 17:28:15 +0800

    🚀 feat: init

--- src/views/index/Home.vue
+++ src/views/index/Home.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div class="left-board">
       <div class="logo-wrapper">
-        <div class="logo">Form-Render</div>
+        <div class="logo">
+          Form-Render
+        </div>
       </div>
       <el-scrollbar class="left-scrollbar">
         <div class="components-list">
@@ -126,61 +128,61 @@
       :show-file-name="showFileName"
       @confirm="generate"
     />
-    <input id="copyNode" type="hidden" />
+    <input id="copyNode" type="hidden">
   </div>
 </template>
 
 <script>
-import draggable from "vuedraggable";
-import { debounce } from "throttle-debounce";
-import { saveAs } from "file-saver";
-import ClipboardJS from "clipboard";
-import render from "@/components/render/render";
-import FormDrawer from "./FormDrawer";
-import JsonDrawer from "./JsonDrawer";
-import RightPanel from "./RightPanel";
+import draggable from 'vuedraggable'
+import { debounce } from 'throttle-debounce'
+import { saveAs } from 'file-saver'
+import ClipboardJS from 'clipboard'
+import render from '@/components/render/render'
+import FormDrawer from './FormDrawer'
+import JsonDrawer from './JsonDrawer'
+import RightPanel from './RightPanel'
 import {
   inputComponents,
   selectComponents,
   layoutComponents,
-  formConf,
-} from "@/components/generator/config";
+  formConf
+} from '@/components/generator/config'
 import {
   exportDefault,
   beautifierConf,
   isNumberStr,
   titleCase,
   deepClone,
-  isObjectObject,
-} from "@/utils/index";
+  isObjectObject
+} from '@/utils/index'
 import {
   makeUpHtml,
   vueTemplate,
   vueScript,
-  cssStyle,
-} from "@/components/generator/html";
-import { makeUpJs } from "@/components/generator/js";
-import { makeUpCss } from "@/components/generator/css";
-import drawingDefalut from "@/components/generator/drawingDefalut";
-import logo from "@/assets/logo.png";
-import CodeTypeDialog from "./CodeTypeDialog";
-import DraggableItem from "./DraggableItem";
+  cssStyle
+} from '@/components/generator/html'
+import { makeUpJs } from '@/components/generator/js'
+import { makeUpCss } from '@/components/generator/css'
+import drawingDefalut from '@/components/generator/drawingDefalut'
+import logo from '@/assets/logo.png'
+import CodeTypeDialog from './CodeTypeDialog'
+import DraggableItem from './DraggableItem'
 import {
   getDrawingList,
   saveDrawingList,
   getIdGlobal,
   saveIdGlobal,
-  getFormConf,
-} from "@/utils/db";
-import loadBeautifier from "@/utils/loadBeautifier";
+  getFormConf
+} from '@/utils/db'
+import loadBeautifier from '@/utils/loadBeautifier'
 
-let beautifier;
-const emptyActiveData = { style: {}, autosize: {} };
-let oldActiveId;
-let tempActiveData;
-const drawingListInDB = getDrawingList();
-const formConfInDB = getFormConf();
-const idGlobal = getIdGlobal();
+let beautifier
+const emptyActiveData = { style: {}, autosize: {} }
+let oldActiveId
+let tempActiveData
+const drawingListInDB = getDrawingList()
+const formConfInDB = getFormConf()
+const idGlobal = getIdGlobal()
 
 export default {
   components: {
@@ -190,7 +192,7 @@ export default {
     JsonDrawer,
     RightPanel,
     CodeTypeDialog,
-    DraggableItem,
+    DraggableItem
   },
   data() {
     return {
@@ -215,293 +217,290 @@ export default {
       saveIdGlobalDebounce: debounce(340, saveIdGlobal),
       leftComponents: [
         {
-          title: "输入型组件",
-          list: inputComponents,
+          title: '输入型组件',
+          list: inputComponents
         },
         {
-          title: "选择型组件",
-          list: selectComponents,
+          title: '选择型组件',
+          list: selectComponents
         },
         {
-          title: "布局型组件",
-          list: layoutComponents,
-        },
-      ],
-    };
+          title: '布局型组件',
+          list: layoutComponents
+        }
+      ]
+    }
   },
   computed: {},
   watch: {
     // eslint-disable-next-line func-names
-    "activeData.__config__.label": function (val, oldVal) {
+    'activeData.__config__.label': function (val, oldVal) {
       if (
-        this.activeData.placeholder === undefined ||
-        !this.activeData.__config__.tag ||
-        oldActiveId !== this.activeId
+        this.activeData.placeholder === undefined
+        || !this.activeData.__config__.tag
+        || oldActiveId !== this.activeId
       ) {
-        return;
+        return
       }
-      this.activeData.placeholder =
-        this.activeData.placeholder.replace(oldVal, "") + val;
+      this.activeData.placeholder = this.activeData.placeholder.replace(oldVal, '') + val
     },
     activeId: {
       handler(val) {
-        oldActiveId = val;
+        oldActiveId = val
       },
-      immediate: true,
+      immediate: true
     },
     drawingList: {
       handler(val) {
-        this.saveDrawingListDebounce(val);
-        if (val.length === 0) this.idGlobal = 100;
+        this.saveDrawingListDebounce(val)
+        if (val.length === 0) this.idGlobal = 100
       },
-      deep: true,
+      deep: true
     },
     idGlobal: {
       handler(val) {
-        this.saveIdGlobalDebounce(val);
+        this.saveIdGlobalDebounce(val)
       },
-      immediate: true,
-    },
+      immediate: true
+    }
   },
   mounted() {
     if (Array.isArray(drawingListInDB) && drawingListInDB.length > 0) {
-      this.drawingList = drawingListInDB;
+      this.drawingList = drawingListInDB
     } else {
-      this.drawingList = drawingDefalut;
+      this.drawingList = drawingDefalut
     }
-    this.activeFormItem(this.drawingList[0]);
+    this.activeFormItem(this.drawingList[0])
     if (formConfInDB) {
-      this.formConf = formConfInDB;
+      this.formConf = formConfInDB
     }
-    loadBeautifier((btf) => {
-      beautifier = btf;
-    });
-    const clipboard = new ClipboardJS("#copyNode", {
-      text: (trigger) => {
-        const codeStr = this.generateCode();
+    loadBeautifier(btf => {
+      beautifier = btf
+    })
+    const clipboard = new ClipboardJS('#copyNode', {
+      text: trigger => {
+        const codeStr = this.generateCode()
         this.$notify({
-          title: "成功",
-          message: "代码已复制到剪切板，可粘贴。",
-          type: "success",
-        });
-        return codeStr;
-      },
-    });
-    clipboard.on("error", (e) => {
-      this.$message.error("代码复制失败");
-    });
+          title: '成功',
+          message: '代码已复制到剪切板，可粘贴。',
+          type: 'success'
+        })
+        return codeStr
+      }
+    })
+    clipboard.on('error', e => {
+      this.$message.error('代码复制失败')
+    })
   },
   methods: {
     setObjectValueReduce(obj, strKeys, data) {
-      const arr = strKeys.split(".");
+      const arr = strKeys.split('.')
       arr.reduce((pre, item, i) => {
         if (arr.length === i + 1) {
-          pre[item] = data;
+          pre[item] = data
         } else if (!isObjectObject(pre[item])) {
-          pre[item] = {};
+          pre[item] = {}
         }
-        return pre[item];
-      }, obj);
+        return pre[item]
+      }, obj)
     },
     setRespData(component, resp) {
-      const { dataPath, renderKey, dataConsumer } = component.__config__;
-      if (!dataPath || !dataConsumer) return;
+      const { dataPath, renderKey, dataConsumer } = component.__config__
+      if (!dataPath || !dataConsumer) return
       const respData = dataPath
-        .split(".")
-        .reduce((pre, item) => pre[item], resp);
+        .split('.')
+        .reduce((pre, item) => pre[item], resp)
 
       // 将请求回来的数据，赋值到指定属性。
       // 以el-tabel为例，根据Element文档，应该将数据赋值给el-tabel的data属性，所以dataConsumer的值应为'data';
       // 此时赋值代码可写成 component[dataConsumer] = respData；
       // 但为支持更深层级的赋值（如：dataConsumer的值为'options.data'）,使用setObjectValueReduce
-      this.setObjectValueReduce(component, dataConsumer, respData);
+      this.setObjectValueReduce(component, dataConsumer, respData)
       const i = this.drawingList.findIndex(
-        (item) => item.__config__.renderKey === renderKey
-      );
-      if (i > -1) this.$set(this.drawingList, i, component);
+        item => item.__config__.renderKey === renderKey
+      )
+      if (i > -1) this.$set(this.drawingList, i, component)
     },
     fetchData(component) {
-      const { dataType, method, url } = component.__config__;
-      if (dataType === "dynamic" && method && url) {
-        this.setLoading(component, true);
+      const { dataType, method, url } = component.__config__
+      if (dataType === 'dynamic' && method && url) {
+        this.setLoading(component, true)
         this.$axios({
           method,
-          url,
-        }).then((resp) => {
-          this.setLoading(component, false);
-          this.setRespData(component, resp.data);
-        });
+          url
+        }).then(resp => {
+          this.setLoading(component, false)
+          this.setRespData(component, resp.data)
+        })
       }
     },
     setLoading(component, val) {
-      const { directives } = component;
+      const { directives } = component
       if (Array.isArray(directives)) {
-        const t = directives.find((d) => d.name === "loading");
-        if (t) t.value = val;
+        const t = directives.find(d => d.name === 'loading')
+        if (t) t.value = val
       }
     },
     activeFormItem(currentItem) {
-      this.activeData = currentItem;
-      this.activeId = currentItem.__config__.formId;
+      this.activeData = currentItem
+      this.activeId = currentItem.__config__.formId
     },
     onEnd(obj) {
       if (obj.from !== obj.to) {
-        this.fetchData(tempActiveData);
-        this.activeData = tempActiveData;
-        this.activeId = this.idGlobal;
+        this.fetchData(tempActiveData)
+        this.activeData = tempActiveData
+        this.activeId = this.idGlobal
       }
     },
     addComponent(item) {
-      const clone = this.cloneComponent(item);
-      this.fetchData(clone);
-      this.drawingList.push(clone);
-      this.activeFormItem(clone);
+      const clone = this.cloneComponent(item)
+      this.fetchData(clone)
+      this.drawingList.push(clone)
+      this.activeFormItem(clone)
     },
     cloneComponent(origin) {
-      const clone = deepClone(origin);
-      const config = clone.__config__;
-      config.span = this.formConf.span; // 生成代码时，会根据span做精简判断
-      this.createIdAndKey(clone);
-      clone.placeholder !== undefined && (clone.placeholder += config.label);
-      tempActiveData = clone;
-      return tempActiveData;
+      const clone = deepClone(origin)
+      const config = clone.__config__
+      config.span = this.formConf.span // 生成代码时，会根据span做精简判断
+      this.createIdAndKey(clone)
+      clone.placeholder !== undefined && (clone.placeholder += config.label)
+      tempActiveData = clone
+      return tempActiveData
     },
     createIdAndKey(item) {
-      const config = item.__config__;
-      config.formId = ++this.idGlobal;
-      config.renderKey = `${config.formId}${+new Date()}`; // 改变renderKey后可以实现强制更新组件
-      if (config.layout === "colFormItem") {
-        item.__vModel__ = `field${this.idGlobal}`;
-      } else if (config.layout === "rowFormItem") {
-        config.componentName = `row${this.idGlobal}`;
-        !Array.isArray(config.children) && (config.children = []);
-        delete config.label; // rowFormItem无需配置label属性
+      const config = item.__config__
+      config.formId = ++this.idGlobal
+      config.renderKey = `${config.formId}${+new Date()}` // 改变renderKey后可以实现强制更新组件
+      if (config.layout === 'colFormItem') {
+        item.__vModel__ = `field${this.idGlobal}`
+      } else if (config.layout === 'rowFormItem') {
+        config.componentName = `row${this.idGlobal}`
+        !Array.isArray(config.children) && (config.children = [])
+        delete config.label // rowFormItem无需配置label属性
       }
       if (Array.isArray(config.children)) {
-        config.children = config.children.map((childItem) =>
-          this.createIdAndKey(childItem)
-        );
+        config.children = config.children.map(childItem => this.createIdAndKey(childItem))
       }
-      return item;
+      return item
     },
     AssembleFormData() {
       this.formData = {
         fields: deepClone(this.drawingList),
-        ...this.formConf,
-      };
+        ...this.formConf
+      }
     },
     generate(data) {
-      const func = this[`exec${titleCase(this.operationType)}`];
-      this.generateConf = data;
-      func && func(data);
+      const func = this[`exec${titleCase(this.operationType)}`]
+      this.generateConf = data
+      func && func(data)
     },
     execRun(data) {
-      this.AssembleFormData();
-      this.drawerVisible = true;
+      this.AssembleFormData()
+      this.drawerVisible = true
     },
     execDownload(data) {
-      const codeStr = this.generateCode();
-      const blob = new Blob([codeStr], { type: "text/plain;charset=utf-8" });
-      saveAs(blob, data.fileName);
+      const codeStr = this.generateCode()
+      const blob = new Blob([codeStr], { type: 'text/plain;charset=utf-8' })
+      saveAs(blob, data.fileName)
     },
     execCopy(data) {
-      document.getElementById("copyNode").click();
+      document.getElementById('copyNode').click()
     },
     empty() {
-      this.$confirm("确定要清空所有组件吗？", "提示", { type: "warning" }).then(
+      this.$confirm('确定要清空所有组件吗？', '提示', { type: 'warning' }).then(
         () => {
-          this.drawingList = [];
-          this.idGlobal = 100;
+          this.drawingList = []
+          this.idGlobal = 100
         }
-      );
+      )
     },
     drawingItemCopy(item, list) {
-      let clone = deepClone(item);
-      clone = this.createIdAndKey(clone);
-      list.push(clone);
-      this.activeFormItem(clone);
+      let clone = deepClone(item)
+      clone = this.createIdAndKey(clone)
+      list.push(clone)
+      this.activeFormItem(clone)
     },
     drawingItemDelete(index, list) {
-      list.splice(index, 1);
+      list.splice(index, 1)
       this.$nextTick(() => {
-        const len = this.drawingList.length;
+        const len = this.drawingList.length
         if (len) {
-          this.activeFormItem(this.drawingList[len - 1]);
+          this.activeFormItem(this.drawingList[len - 1])
         }
-      });
+      })
     },
     generateCode() {
-      const { type } = this.generateConf;
-      this.AssembleFormData();
-      console.log(this.formData, "this.formData");
-      const script = vueScript(makeUpJs(this.formData, type));
-      const html = vueTemplate(makeUpHtml(this.formData, type));
-      const css = cssStyle(makeUpCss(this.formData));
-      return beautifier.html(html + script + css, beautifierConf.html);
+      const { type } = this.generateConf
+      this.AssembleFormData()
+      console.log(this.formData, 'this.formData')
+      const script = vueScript(makeUpJs(this.formData, type))
+      const html = vueTemplate(makeUpHtml(this.formData, type))
+      const css = cssStyle(makeUpCss(this.formData))
+      return beautifier.html(html + script + css, beautifierConf.html)
     },
     showJson() {
-      this.AssembleFormData();
-      this.jsonDrawerVisible = true;
+      this.AssembleFormData()
+      this.jsonDrawerVisible = true
     },
     download() {
-      this.dialogVisible = true;
-      this.showFileName = true;
-      this.operationType = "download";
+      this.dialogVisible = true
+      this.showFileName = true
+      this.operationType = 'download'
     },
     run() {
-      this.dialogVisible = true;
-      this.showFileName = false;
-      this.operationType = "run";
+      this.dialogVisible = true
+      this.showFileName = false
+      this.operationType = 'run'
     },
     copy() {
-      this.dialogVisible = true;
-      this.showFileName = false;
-      this.operationType = "copy";
+      this.dialogVisible = true
+      this.showFileName = false
+      this.operationType = 'copy'
     },
     tagChange(newTag) {
-      newTag = this.cloneComponent(newTag);
-      const config = newTag.__config__;
-      newTag.__vModel__ = this.activeData.__vModel__;
-      config.formId = this.activeId;
-      config.span = this.activeData.__config__.span;
-      this.activeData.__config__.tag = config.tag;
-      this.activeData.__config__.tagIcon = config.tagIcon;
-      this.activeData.__config__.document = config.document;
+      newTag = this.cloneComponent(newTag)
+      const config = newTag.__config__
+      newTag.__vModel__ = this.activeData.__vModel__
+      config.formId = this.activeId
+      config.span = this.activeData.__config__.span
+      this.activeData.__config__.tag = config.tag
+      this.activeData.__config__.tagIcon = config.tagIcon
+      this.activeData.__config__.document = config.document
       if (
-        typeof this.activeData.__config__.defaultValue ===
-        typeof config.defaultValue
+        typeof this.activeData.__config__.defaultValue
+        === typeof config.defaultValue
       ) {
-        config.defaultValue = this.activeData.__config__.defaultValue;
+        config.defaultValue = this.activeData.__config__.defaultValue
       }
-      Object.keys(newTag).forEach((key) => {
+      Object.keys(newTag).forEach(key => {
         if (this.activeData[key] !== undefined) {
-          newTag[key] = this.activeData[key];
+          newTag[key] = this.activeData[key]
         }
-      });
-      this.activeData = newTag;
-      this.updateDrawingList(newTag, this.drawingList);
+      })
+      this.activeData = newTag
+      this.updateDrawingList(newTag, this.drawingList)
     },
     updateDrawingList(newTag, list) {
       const index = list.findIndex(
-        (item) => item.__config__.formId === this.activeId
-      );
+        item => item.__config__.formId === this.activeId
+      )
       if (index > -1) {
-        list.splice(index, 1, newTag);
+        list.splice(index, 1, newTag)
       } else {
-        list.forEach((item) => {
+        list.forEach(item => {
           if (Array.isArray(item.__config__.children)) {
-            this.updateDrawingList(newTag, item.__config__.children);
+            this.updateDrawingList(newTag, item.__config__.children)
           }
-        });
+        })
       }
     },
     refreshJson(data) {
-      this.drawingList = deepClone(data.fields);
-      delete data.fields;
-      this.formConf = data;
-    },
-  },
-};
+      this.drawingList = deepClone(data.fields)
+      delete data.fields
+      this.formConf = data
+    }
+  }
+}
 </script>
 
 <style lang="scss">
